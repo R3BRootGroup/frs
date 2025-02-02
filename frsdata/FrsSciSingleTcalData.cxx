6c5cffd39ba0df5678efa6e96c12510d09582959
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2025-02-02 11:06:59 +0100

    Cleaning new branch

--- frsdata/FrsSciSingleTcalData.cxx
+++
@@ -1,5 +0,0 @@
-#include "FrsSciSingleTcalData.h"
-
-FrsSciSingleTcalData::FrsSciSingleTcalData() {}
-
-ClassImp(FrsSciSingleTcalData)
--- macros/geo/rootlogon.C
+++
@@ -1,8 +0,0 @@
-
-{
-    gSystem->Load("libGenVector");
-
-    cout << "[info] geometry macros for WASA@FRS" << endl;
-    cout << "[info] .L macro.C" << endl;
-    cout << "[info] macro(here options)" << endl;
-}
