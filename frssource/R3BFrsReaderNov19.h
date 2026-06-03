2cbf3393afd255d402ce9dcc6a3fd82f7629befa
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-06-03 17:19:15 +0200

    Change read function to r3bread

--- frssource/R3BFrsReaderNov19.h
+++ frssource/R3BFrsReaderNov19.h
@@ -32,7 +32,7 @@ class R3BFrsReaderNov19 : public R3BReader
     ~R3BFrsReaderNov19();
 
     Bool_t Init(ext_data_struct_info*);
-    Bool_t Read();
+    Bool_t R3BRead();
     void Reset();
 
     /** Accessor to select online mode **/
